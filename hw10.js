c7267eb235a8106e0ca68d7be7c37261d8576c7a
NikolaiTiutrin <89229569+NikolaiTiutrin@users.noreply.github.com>
2021-10-13 22:15:36 +1100

    Update hw10.js

--- hw10.js
+++ hw10.js
@@ -2,3 +2,7 @@ var vg_1 = "hw10.json";
 vegaEmbed("#map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_2 = "UFOtimes.json";
+vegaEmbed ("heatmap", vg_2).then(function(result){
+   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
